399bcc1b44dad14d55b36e2f0d6ffc0f1959ebf6
Sajalrastogi89 <Sajalrastogii89@gmail.com>
2023-12-12 00:07:57 +0530

    hello

---
+++ config.js
@@ -0,0 +1,33 @@
+var config = {
+    texts: [
+        "Happy Birthday Swati Mam!", 
+        "Best Mam",
+        "On this special day,",
+        "I want to express my gratitude",
+        "for being such a kind and wonderful teacher.",
+        "Your kindness and guidance",
+        "have made a lasting impact on me.",
+        "May your day be filled with joy,",
+        "laughter, and all the things",
+        "that make you happy.",
+        "Thank you for being my favorite teacher!",
+        "Wishing you a year ahead",
+        "filled with success and happiness.",
+        "Cheers to you, Swati Mam!",
+        "🥳🎊",
+        "Swati Mam ~~"
+    ],
+    imgs: {
+        "Best Mam": "./imgs/Screenshot 2023-08-31 133120.png",
+    },
+    desc: {
+        turn_on: "Begin",
+        play: "Music",
+        bannar_coming: "Color",
+        balloons_flying: "Oh, it seems like something is missing.",
+        cake_fadein: "Cake?",
+        light_candle: "Candle?",
+        wish_message: "Happy birthday",
+        story: "A MESSAGE FOR YOU",
+    }
+};
